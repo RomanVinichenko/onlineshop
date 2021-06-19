4b19b052ebd388604110a9ea274d1ff35eeb5500
Максим Сергеевич <krovorgen@gmail.com>
2021-06-19 22:40:38 +0300

    added mixins

--- src/components/ModalBuy/index.tsx
+++ src/components/ModalBuy/index.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, useState } from 'react';
+import React, { useState } from 'react';
 import emailjs from 'emailjs-com';
 import PhoneInput from 'react-phone-number-input';
 
