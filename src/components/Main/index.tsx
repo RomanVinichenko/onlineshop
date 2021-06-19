46c14d9936a5e72f01d3e2b13b41371373237b1e
Roman Vinichenko <chicagezzz@gmail.com>
2021-06-20 00:38:16 +0300

    added adaptive

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from 'react';
+import React, {FC, useState} from 'react';
 
 import Header from '../Header';
 import Products from '../Products';
@@ -10,105 +10,109 @@ import styles from './style.module.scss';
 const Main: FC = () => {
   let [openPayModal, setOpenPayModal] = useState<boolean>(false);
   return (
-    <>
-      <Header/>
-      <section className={styles['main']}>
-        <div className={'container'}>
-          <div className={styles['main__inner']}>
-            <div className={styles['main__description']}>
-              <h1 className={styles['main__title']}>
-                СКИДКА НА ВЫБРАННЫЕ ТОВАРЫ ДО <span>50%!</span> Успей заказать, осталось всего:
-              </h1>
-              <p className={styles['main__subtitle']}>
-                Бесплатная доставка при заказе от 2-х комплектов!
-              </p>
-              <div className={styles['main__decor']}>
-                <ul className={styles['main__list']}>
-                  <li className={styles['main__list-item']}>
-                    <img
-                      className={styles['main__list-img']}
-                      src={
-                        process.env.PUBLIC_URL + '/assets/images/cotton.png'
-                      }
-                      alt="decor"
-                    />
-                    <p className={styles['main__list-text']}>
-                      100%-й хлопок высокой плотности
-                    </p>
-                  </li>
-                  <li className={styles['main__list-item']}>
-                    <img
-                      className={styles['main__list-img']}
-                      src={
-                        process.env.PUBLIC_URL + '/assets/images/change.png'
-                      }
-                      alt="decor"
-                    />
-                    <p className={styles['main__list-text']}>
-                      Возврат или обмен в течение 14 дней
-                    </p>
-                  </li>
-                  <li className={styles['main__list-item']}>
-                    <img
-                      className={styles['main__list-img']}
-                      src={process.env.PUBLIC_URL + '/assets/images/gift.png'}
-                      alt="decor"
-                    />
-                    <p className={styles['main__list-text']}>
-                      К каждому комплекту в заказе подарок
-                    </p>
-                  </li>
-                </ul>
+      <>
+          <Header />
+          <section className={styles['main']}>
+              <div className={'container'}>
+                  <div className={styles['main__inner']}>
+                      <div className={styles['main__description']}>
+                          <h1 className={styles['main__title']}>
+                              СКИДКА НА ВЫБРАННЫЕ ТОВАРЫ ДО <span>50%!</span> Успей заказать,
+                              осталось всего:
+                          </h1>
+                          <p className={styles['main__subtitle']}>
+                              Бесплатная доставка при заказе от 2-х комплектов!
+                          </p>
+                          <div className={styles['main__decor']}>
+                              <ul className={styles['main__list']}>
+                                  <li className={styles['main__list-item']}>
+                                      <img
+                                          className={styles['main__list-img']}
+                                          src={process.env.PUBLIC_URL + '/assets/images/cotton.png'}
+                                          alt="decor"
+                                      />
+                                      <p className={styles['main__list-text']}>
+                                          100%-й хлопок высокой плотности
+                                      </p>
+                                  </li>
+                                  <li className={styles['main__list-item']}>
+                                      <img
+                                          className={styles['main__list-img']}
+                                          src={process.env.PUBLIC_URL + '/assets/images/change.png'}
+                                          alt="decor"
+                                      />
+                                      <p className={styles['main__list-text']}>
+                                          Возврат или обмен в течение 14 дней
+                                      </p>
+                                  </li>
+                                  <li className={styles['main__list-item']}>
+                                      <img
+                                          className={styles['main__list-img']}
+                                          src={process.env.PUBLIC_URL + '/assets/images/gift.png'}
+                                          alt="decor"
+                                      />
+                                      <p className={styles['main__list-text']}>
+                                          К каждому комплекту в заказе подарок
+                                      </p>
+                                  </li>
+                              </ul>
+                          </div>
+                          <button
+                              className={styles['main__btn']}
+                              onClick={() => setOpenPayModal(true)}>
+                              Заказать и получить скидку
+                          </button>
+                      </div>
+                      <div>
+                          <img
+                              className={styles['main__pic']}
+                              src={process.env.PUBLIC_URL + '/assets/images/2.31.jpg'}
+                              alt="top image"
+                          />
+                          <div className={styles['main__square']}></div>
+                      </div>
+                  </div>
               </div>
-              <button className={styles['main__btn']} onClick={() => setOpenPayModal(true)}>
-                Заказать и получить скидку
-              </button>
-            </div>
-            <div>
-              <img
-                className={styles['main__pic']}
-                src={process.env.PUBLIC_URL + '/assets/images/2.31.jpg'}
-                alt="top image"
-              />
-              <div className={styles['main__square']}></div>
-            </div>
-          </div>
-        </div>
-      </section>
+          </section>
 
-      <Products/>
-      {openPayModal && (
-        <Modal callBack={() => setOpenPayModal(false)}>
-          <ModalBuy />
-        </Modal>
-      )}
+          <Products />
+          {openPayModal && (
+              <Modal callBack={() => setOpenPayModal(false)}>
+                  <ModalBuy />
+              </Modal>
+          )}
 
-      <section className={styles['about']}>
-        <div className={styles['container']}>
-          <div className={styles['about__inner']}>
-            <h2 className={styles['about__title']}>
-              О доставке
-            </h2>
-            <ul className={'about__items'}>
-              <li className={'about__item'}>
-                Мы доставляем товары с нашего магазина по всей территории Украины
-              </li>
-              <li className={'about__item'}>
-                Доставка по Новой Почте от 1-го до 3-ех дней
-              </li>
-              <li className={'about__item'}>
-                Отправляем наложенным платежом
-              </li>
-              <li className={'about__item'}>
-                Мы доставляем товары с нашего магазина по всей территории Украины
-              </li>
-            </ul>
-
-
-          </div>
-        </div>
-      </section>
-    </>
+          <section className={styles['about']}>
+              <div className={'container'}>
+                  <div className={styles['about__inner']}>
+                      <h2 className={styles['about__title']}>О доставке</h2>
+                      <ul className={styles['about__items']}>
+                          <li className={styles['about__item']}>
+                              <p className={styles['about__text']}>
+                                  Мы доставляем товары с нашего магазина по всей территории Украины
+                              </p>
+                          </li>
+                          <li className={styles['about__item']}>
+                              <p className={styles['about__text']}>
+                                  Доставка по Новой Почте от 1-го до 3-ех дней
+                              </p>
+                          </li>
+                          <li className={styles['about__item']}>
+                              <p className={styles['about__text']}>
+                                  Отправляем наложенным платежом и оплата осущетвляется только при
+                                  получении товара
+                              </p>
+                          </li>
+                          <li className={styles['about__item']}>
+                              <p className={styles['about__text']}>
+                                  Все наши товары соответствуют картинке и описанию на сайте
+                              </p>
+                          </li>
+                      </ul>
+                  </div>
+              </div>
+          </section>
+      </>
   );
 };
 
