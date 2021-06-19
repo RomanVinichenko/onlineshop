348d90c3dc4c70ff0a328be01085315f0e9092bc
Максим Сергеевич <krovorgen@gmail.com>
2021-06-19 16:50:55 +0300

    added modal

--- src/components/Products/index.tsx
+++ src/components/Products/index.tsx
@@ -1,35 +1,57 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 
 import mockData from '../../mockDataProduct';
 
 import styles from './style.module.scss';
 
 const Products: FC = () => {
+    const [modalContent, setModalContent] = useState<any>(null);
+
+    const modalOpenHandler = (img: string, title: string, price: string, size: JSX.Element | string) => {
+        setModalContent({ img, title, price, size });
+    };
+    console.log(modalContent);
     return (
-        <section className={styles['products']}>
-            <div className={'container'}>
-                <h2 className={styles['products__title']}>Товары</h2>
-                <div className={styles['products__inner']}>
-                    {mockData.map((item) => {
-                        return (
-                            <article key={item.id} className={'card'}>
-                                <img
-                                    className={'card__img'}
-                                    src={`${process.env.PUBLIC_URL}/assets/images/${item.img}`}
-                                    alt="Логотип товара"
-                                />
-                                <h2 className={'card__title'}>{item.title} Название</h2>
-                                <span className={'card__price'}>{item.price}</span>
-                                <div className={'buttons'}>
-                                    <button className={'card__btn-more'}>Подробнее</button>
-                                    <button className={'card__btn-buy'}>Купить</button>
-                                </div>
-                            </article>
-                        );
-                    })}
+        <>
+            <section className={styles['products']}>
+                <div className={'container'}>
+                    <h2 className={styles['products__title']}>Товары</h2>
+                    <div className={styles['products__inner']}>
+                        {mockData.map((item) => {
+                            return (
+                                <article key={item.id} className={'card'}>
+                                    <img
+                                        className={'card__img'}
+                                        src={`${process.env.PUBLIC_URL}/assets/images/${item.img}`}
+                                        alt="Логотип товара"
+                                    />
+                                    <h2 className={'card__title'}>{item.title} Название</h2>
+                                    <span className={'card__price'}>{item.price}</span>
+                                    <div className={'buttons'}>
+                                        <button className={'card__btn-more'} onClick={() => modalOpenHandler(item.img, item.title, item.price, item.description.text)}>Подробнее</button>
+                                        <button className={'card__btn-buy'}>Купить</button>
+                                    </div>
+                                </article>
+                            );
+                        })}
+                    </div>
                 </div>
-            </div>
-        </section>
+            </section>
+            {modalContent && (
+                <div className={styles['modal']}>
+                    <div className={styles['modal__content']}>
+                        <div className={styles['modal__box']}>
+                            <img src={`${process.env.PUBLIC_URL}/assets/images/${modalContent?.img}`} alt='' />
+                            <p>{modalContent?.title}</p>
+                            <p>{modalContent?.price}</p>
+                            <p>{modalContent?.size}</p>
+                            <button className={styles['modal__close']} onClick={() => setModalContent(null)}>X</button>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </>
+
     );
 };
 
