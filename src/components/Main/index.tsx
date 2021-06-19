5db6be1d0cc5c411be044a1411a059602db4fdac
Максим Сергеевич <krovorgen@gmail.com>
2021-06-19 12:52:27 +0300

    Fixed styles and jsx

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,41 +1,41 @@
-import React, {FC} from 'react';
+import React, { FC } from 'react';
+
+import logo from '../../assets/icons/logo.svg';
 
 import styles from './style.module.scss';
 
 const Main: FC = () => {
-  return (
-
-    <header className={styles['header']}>
-      <div className={styles['container']}>
-        <nav className={styles['menu']}>
-          <a className={styles.logo} href="#">
-            <img className={styles.header__img} src="../../assets/icons/logo.svg" alt="Логотип"/>
-          </a>
-          <ul className={styles.menu__list}>
-            <li className={styles.menu__listItem}>
-              <a className={styles.menu__listLink} href="#">
-                Товары
-              </a>
-            </li>
-            <li className={styles.menu__listItem}>
-              <a className={styles.menu__listLink} href="#">
-                О нас
-              </a>
-            </li>
-          </ul>
-          <a className={styles.menu__contact} href="#">
-            <img className={styles.header__img} src="images/logo.svg" alt="Телефон"/>
-          </a>
-        </nav>
-      </div>
-
-      <section className={styles.main}>
-        <h1 className={styles.main__title}>
-          СКИДКА НА ВЫБРАННЫЕ ТОВАРЫ ДО 50%! Успей заказать, осталось всего:
-        </h1>
-      </section>
-    </header>
-  );
+    // TODO: del header--active
+    return (
+        <>
+            <header className={`${styles['header']} ${styles['header--active']}`}>
+                <div className={styles['container']}>
+                    <nav className={styles['menu']}>
+                        <a className={styles.logo} href="#">
+                            <img className={styles.header__img} src={logo} alt="Логотип" />
+                        </a>
+                        <ul className={styles.menu__list}>
+                            <li className={styles.menu__listItem}>
+                                <a className={styles.menu__listLink} href="#">
+                                    Товары
+                                </a>
+                            </li>
+                            <li className={styles.menu__listItem}>
+                                <a className={styles.menu__listLink} href="#">
+                                    О нас
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </header>
+            <section className={styles.main}>
+                <h1 className={styles.main__title}>
+                    СКИДКА НА ВЫБРАННЫЕ ТОВАРЫ ДО 50%! Успей заказать, осталось всего:
+                </h1>
+            </section>
+        </>
+    );
 };
 
 export default Main;
