128d43ac9cfb316b09d2edabe03a10efcf26d171
Roman Vinichenko <chicagezzz@gmail.com>
2021-06-21 21:49:12 +0300

    added select field

--- src/components/ModalBuy/index.tsx
+++ src/components/ModalBuy/index.tsx
@@ -1,82 +1,83 @@
-import React, {ChangeEvent, FC, FormEvent, useState} from 'react';
+import React, { ChangeEvent, FC, FormEvent, useState } from 'react';
 import emailjs from 'emailjs-com';
 import PhoneInput from 'react-phone-number-input';
 
 import styles from './style.module.scss';
 
 interface IModalBuyProps {
-  greetingUser?: string;
+    greetingUser?: string;
 }
 
-const ModalBuy: FC<IModalBuyProps> = ({greetingUser}) => {
-  const [value, setValue] = useState('');
-  const [textareaValue, setTextareaValue] = useState(greetingUser);
+const ModalBuy: FC<IModalBuyProps> = ({ greetingUser }) => {
+    const [value, setValue] = useState('');
+    const [textareaValue, setTextareaValue] = useState(greetingUser);
 
-  const textareaValueHandler = (e: ChangeEvent<HTMLTextAreaElement>) => {
-    setTextareaValue(e.currentTarget.value);
-  };
+    const textareaValueHandler = (e: ChangeEvent<HTMLTextAreaElement>) => {
+        setTextareaValue(e.currentTarget.value);
+    };
 
-  const [buttonText, setButtonText] = useState('Отправить заявку');
+    const [buttonText, setButtonText] = useState('Отправить заявку');
 
-  function sendEmail(e: FormEvent<HTMLFormElement>) {
-    e.preventDefault();
-    const element = e.target as HTMLFormElement;
+    function sendEmail(e: FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        const element = e.target as HTMLFormElement;
 
-    emailjs
-      .sendForm('service_zwzcuj8', 'template_nnofpol', element, 'user_lMzDOCxB7FMfmgkx2Qr4C')
-      .then();
+        emailjs
+            .sendForm('service_zwzcuj8', 'template_nnofpol', element, 'user_lMzDOCxB7FMfmgkx2Qr4C')
+            .then();
 
-    element.reset();
-    setTextareaValue('');
-    setButtonText('Сообщение отправлено');
-    setValue('');
-    setTimeout(function () {
-      setButtonText('Отправить заявку');
-    }, 3000);
-    // TODO: CLOSE
-  }
+        element.reset();
+        setTextareaValue('');
+        setButtonText('Сообщение отправлено');
+        setValue('');
+        setTimeout(function () {
+            setButtonText('Отправить заявку');
+        }, 3000);
+        // TODO: CLOSE
+    }
 
-  return (
-    <form className={styles['contact-form']} onSubmit={sendEmail}>
-      <input
-        className={styles['contact-form__name']}
-        type="text"
-        name="name"
-        id="name"
-        placeholder="Ваше имя"
-        required
-      />
-      <PhoneInput
-        className={styles['contact-form__phone']}
-        placeholder="Ваш телефон"
-        international
-        defaultCountry="UA"
-        value={value}
-        onChange={setValue}
-        name="phone"
-        id="phone"
-        required
-      />
+    return (
+        <form className={styles['contact-form']} onSubmit={sendEmail}>
+            <input
+                className={styles['contact-form__name']}
+                type="text"
+                name="name"
+                id="name"
+                placeholder="Ваше имя"
+                required
+            />
+            <PhoneInput
+                className={styles['contact-form__phone']}
+                placeholder="Ваш телефон"
+                international
+                defaultCountry="UA"
+                value={value}
+                onChange={setValue}
+                name="phone"
+                id="phone"
+                required
+            />
+<div className={styles['contact-form__category-inner']}>
+            <select className={styles['contact-form__category']} id="category" required>
+                <option value="Все" selected disabled>
+                    Выберете категорию
+                </option>
+                <option value="">Полотенца</option>
+                <option value="">Постельное белье</option>
+            </select>
+</div>
 
-      <select className={styles['contact-form__category']}
-              placeholder="Выберете категорию"
-              id="category"
-              required>
-        <option value="Все" selected>Все</option>
-        <option value="">Полотенца</option>
-        <option value="">Постельное белье</option>
-      </select>
-
-      <textarea
-        className={styles['contact-form__text']}
-        name="message"
-        id="message"
-        value={textareaValue}
-        onChange={textareaValueHandler}
-      />
-      <input className={styles['contact-form__btn']} type="submit" value={buttonText}/>
-    </form>
-  );
+            <textarea
+                className={styles['contact-form__text']}
+                placeholder="Введите ваше сообщение"
+                name="message"
+                id="message"
+                value={textareaValue}
+                onChange={textareaValueHandler}
+            />
+            <input className={styles['contact-form__btn']} type="submit" value={buttonText} />
+        </form>
+    );
 };
 
 export default ModalBuy;
