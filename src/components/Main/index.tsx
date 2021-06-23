c384da3e945713fba4601f2bfb5e3e7b8793aef6
Roman Vinichenko <chicagezzz@gmail.com>
2021-06-23 15:47:16 +0300

    added animation, fix main screen

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -14,103 +14,88 @@ import { Link } from 'react-scroll';
 import styles from './style.module.scss';
 
 const Main: FC = () => {
-    let [openPayModal, setOpenPayModal] = useState<boolean>(false);
-    return (
-        <>
-            <LazyLoad>
-                <Header />
-            </LazyLoad>
+  let [openPayModal, setOpenPayModal] = useState<boolean>(false);
+  return (
+    <>
+      <LazyLoad>
+        <Header />
+      </LazyLoad>
 
-            <section className={styles['main']} id={'main'}>
-                <div className={'container'}>
-                    <div className={styles['main__inner']}>
-                        <div className={styles['main__description']}>
-                            <h1 className={styles['main__title']}>
-                                СКИДКА НА ВЫБРАННЫЕ ТОВАРЫ ДО <span>50%!</span> Успей заказать,
-                                осталось всего:
-                            </h1>
-                            <p className={styles['main__subtitle']}>
-                                Бесплатная доставка при заказе от 4-ех единиц товаров!
-                            </p>
-                            <div className={styles['main__decor']}>
-                                <ul className={styles['main__list']}>
-                                    <li className={styles['main__list-item']}>
-                                        <img
-                                            className={styles['main__list-img']}
-                                            src={
-                                                process.env.PUBLIC_URL +
-                                                '../assets/images/cotton.svg'
-                                            }
-                                            alt="картинка хлопок"
-                                        />
-                                        <p className={styles['main__list-text']}>
-                                            100%-й хлопок высокой плотности
-                                        </p>
-                                    </li>
-                                    <li className={styles['main__list-item']}>
-                                        <img
-                                            className={styles['main__list-img']}
-                                            src={
-                                                process.env.PUBLIC_URL +
-                                                '../assets/images/change.svg'
-                                            }
-                                            alt="картинка обмен"
-                                        />
-                                        <p className={styles['main__list-text']}>
-                                            Возврат или обмен в течение 14 дней
-                                        </p>
-                                    </li>
-                                    <li className={styles['main__list-item']}>
-                                        <img
-                                            className={styles['main__list-img']}
-                                            src={
-                                                process.env.PUBLIC_URL + '../assets/images/gift.svg'
-                                            }
-                                            alt="картинка подарок"
-                                        />
-                                        <p className={styles['main__list-text']}>
-                                            При покупке постельного + полотенца, вы получаете 10%
-                                            скидки на следующий заказ
-                                        </p>
-                                    </li>
-                                </ul>
-                            </div>
-                            <button
-                                className={styles['main__btn']}
-                                onClick={() => setOpenPayModal(true)}>
-                                Заказать и получить скидку
-                            </button>
-                        </div>
-                        <div>
-                            <img
-                                className={styles['main__pic']}
-                                src={process.env.PUBLIC_URL + '/assets/images/top-img.jpg'}
-                                alt="Диван"
-                            />
-                        </div>
-                    </div>
-                </div>
-            </section>
+      <section className={styles['main']} id={'main'}>
+        <div className={'container'}>
+          <div className={styles['main__inner']}>
+            <div className={styles['main__description']}>
+              <h1 className={styles['main__title']}>
+                СКИДКА НА ВЫБРАННЫЕ ТОВАРЫ ДО <span>50%!</span> Успей заказать, осталось всего:
+              </h1>
+              <p className={styles['main__subtitle']}>
+                Бесплатная доставка при заказе от 4-ех единиц товаров!
+              </p>
+              <div className={styles['main__decor']}>
+                <ul className={styles['main__list']}>
+                  <li className={styles['main__list-item']}>
+                    <img
+                      className={styles['main__list-img']}
+                      src={process.env.PUBLIC_URL + '../assets/images/cotton.svg'}
+                      alt="картинка хлопок"
+                    />
+                    <p className={styles['main__list-text']}>100%-й хлопок высокой плотности</p>
+                  </li>
+                  <li className={styles['main__list-item']}>
+                    <img
+                      className={styles['main__list-img']}
+                      src={process.env.PUBLIC_URL + '../assets/images/change.svg'}
+                      alt="картинка обмен"
+                    />
+                    <p className={styles['main__list-text']}>Возврат или обмен в течение 14 дней</p>
+                  </li>
+                  <li className={styles['main__list-item']}>
+                    <img
+                      className={styles['main__list-img']}
+                      src={process.env.PUBLIC_URL + '../assets/images/gift.svg'}
+                      alt="картинка подарок"
+                    />
+                    <p className={styles['main__list-text']}>
+                      При покупке постельного + полотенца, вы получаете 10% скидки на следующий
+                      заказ
+                    </p>
+                  </li>
+                </ul>
+              </div>
+              <button className={styles['main__btn']} onClick={() => setOpenPayModal(true)}>
+                Заказать и получить скидку
+              </button>
+            </div>
+            <div className={styles['main__image']}>
+              <img
+                className={styles['main__pic']}
+                src={process.env.PUBLIC_URL + '/assets/images/top-img.jpg'}
+                alt="Диван"
+              />
+            </div>
+          </div>
+        </div>
+      </section>
 
-            <LazyLoad height={800}>
-                <Products />
-            </LazyLoad>
+      <LazyLoad height={800}>
+        <Products />
+      </LazyLoad>
 
-            <About />
+      <About />
 
-            {openPayModal && (
-                <Modal callBack={() => setOpenPayModal(false)} small>
-                    <ModalBuy />
-                </Modal>
-            )}
+      {openPayModal && (
+        <Modal callBack={() => setOpenPayModal(false)} small>
+          <ModalBuy />
+        </Modal>
+      )}
 
-            <Feedback />
+      <Feedback />
 
-            <Footer />
+      <Footer />
 
-            <Link className={styles['arrow']} to={'main'} smooth={true} duration={1000} />
-        </>
-    );
+      <Link className={styles['arrow']} to={'main'} smooth={true} duration={1000} />
+    </>
+  );
 };
 
 export default Main;
